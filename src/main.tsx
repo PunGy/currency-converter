c784af0e9546f688e13c119cb0f3288f2a656ed5
Maxim Iakovlev <max.yakovlev555@gmail.com>
2022-09-10 11:12:58 +0200

    prepare for moving to rxjs and vite

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <div>123</div>
+    </React.StrictMode>,
+)
--- src/types.ts
+++
@@ -1,7 +0,0 @@
-export type Currency = { code: string, label: string }
-
-export type ExchangeRate = {
-    date: string;
-    currency: string;
-    rate: Record<string, number>;
-}
